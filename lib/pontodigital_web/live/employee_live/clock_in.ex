1fe95635381616743d33b7ac04d37942a91e2658
João Victor Macedo <gmvictorr@outlook.com>
2026-01-27 09:46:01 -0300

    fix: correção do tratamento de atoms, evitando erro por nao inicialização mantendo  a segurança quanto aos ataques por exaustão de memória

--- lib/pontodigital_web/live/employee_live/clock_in.ex
+++ lib/pontodigital_web/live/employee_live/clock_in.ex
@@ -9,20 +9,31 @@ defmodule PontodigitalWeb.EmployeeLive.ClockIn do
 
   @impl true
   def handle_event("registrar_ponto", %{"type" => type_string}, socket) do
-    type = String.to_existing_atom(type_string)
+    type =
+      case type_string do
+        "entrada" -> :entrada
+        "ida_almoco" -> :ida_almoco
+        "volta_almoco" -> :volta_almoco
+        "saida" -> :saida
+        _ -> nil
+      end
 
-    case Timekeeping.register_clock_in(socket.assigns.employee.id, type) do
-      {:ok, _} ->
-        {:noreply, put_flash(socket, :info, "Ponto registrado com sucesso!")}
+    if type do
+      case Timekeeping.register_clock_in(socket.assigns.employee.id, type) do
+        {:ok, _} ->
+          {:noreply, put_flash(socket, :info, "Ponto registrado com sucesso!")}
 
-      {:error, :invalid_sequence, msg} ->
-        {:noreply, put_flash(socket, :error, msg)}
+        {:error, :invalid_sequence, msg} ->
+          {:noreply, put_flash(socket, :error, msg)}
 
-      {:error, :duplicate_entry} ->
-        {:noreply, put_flash(socket, :error, "Registro duplicado.")}
+        {:error, :duplicate_entry} ->
+          {:noreply, put_flash(socket, :error, "Registro duplicado.")}
 
-      {:error, _} ->
-        {:noreply, put_flash(socket, :error, "Erro ao registrar.")}
+        {:error, _} ->
+          {:noreply, put_flash(socket, :error, "Erro ao registrar.")}
+      end
+    else
+      {:noreply, put_flash(socket, :error, "Tipo de ponto inválido!")}
     end
   end
 end
