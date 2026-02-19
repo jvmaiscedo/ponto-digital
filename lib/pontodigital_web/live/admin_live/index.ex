ffe1ddb7e4d014f2843c183eba6d88f1150e0533
Joao Victor Macedo <gmvictorr@outlook.com>
2026-02-19 15:48:47 -0300

    feat: adicao de mensagem de boas-vindas e ajuste estrutural no dashboard admin

--- lib/pontodigital_web/live/admin_live/index.ex
+++ lib/pontodigital_web/live/admin_live/index.ex
@@ -2,16 +2,34 @@ defmodule PontodigitalWeb.AdminLive.Index do
   use PontodigitalWeb, :live_view
 
   alias Pontodigital.Company
-
-
+  alias Pontodigital.Repo
 
   @impl true
   def mount(_params, _session, socket) do
-    current_user = socket.assigns.current_scope.user
+    current_user = Repo.preload(socket.assigns.current_scope.user, :employee)
     total_funcionarios = Company.count_employees()
+
+    primeiro_nome = extrair_primeiro_nome(current_user)
+
     {:ok,
      socket
      |> assign(total_funcionarios: total_funcionarios)
-     |> assign(current_user: current_user) }
+     |> assign(current_user: current_user)
+     |> assign(primeiro_nome: primeiro_nome)}
+  end
+
+  defp extrair_primeiro_nome(%{employee: %Pontodigital.Company.Employee{full_name: nome}}) when not is_nil(nome) do
+    nome
+    |> String.split(" ")
+    |> List.first()
+  end
+
+  defp extrair_primeiro_nome(%{email: email}) do
+    email
+    |> String.split("@")
+    |> List.first()
+    |> String.split(".")
+    |> List.first()
+    |> String.capitalize()
   end
 end
